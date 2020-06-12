c6af1323839b31dea3a4828cc6f8f714710a043d
ahharris <ahharris@tutanota.com>
2020-06-12 14:10:37 +0300

    Новый алго и кнопка

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from tkinter import messagebox, filedialog
 from tkinter.ttk import Combobox
 from conveyor import Conveyor
 from math import factorial
+from datetime import datetime
+from ttkthemes import ThemedStyle
 
 class Application:
     
@@ -16,7 +18,7 @@ class Application:
         self.l2 = Label(window, text="Ініціалізація")
 
         self.algo_choice = Combobox(window)
-        self.algo_choice['values'] = ("Жадібний", "Бджолиний")
+        self.algo_choice['values'] = ("Жадібний", "Бджолиний", "Жадібна Бджілка")
 
         self.init_choice = Combobox(window)
         self.init_choice['values'] = ("Приклад", "Вручну", "Випадкова",
@@ -32,7 +34,7 @@ class Application:
         self.start_button.grid(row=1, column=3)
 
     def choice_exec_way(self):
-        if self.algo_choice.get() != "Жадібний" and self.algo_choice.get() != "Бджолиний":
+        if self.algo_choice.get() != "Жадібний" and self.algo_choice.get() != "Бджолиний" and self.algo_choice.get() != "Жадібна Бджілка":
             self.error_handler("error0")
         else:
 
@@ -182,7 +184,7 @@ class Application:
                     vector.append(spin)
             self.time_matrix.append(vector)
 
-        if self.algo_choice.get() == "Бджолиний":
+        if self.algo_choice.get() != "Жадібний":
             self.l4 = Label(text="Кількість напрямків")
             self.choice_transit = Spinbox(self.window, from_=1, to=factorial(self.Job.number_of_details), width=19)
 
@@ -201,7 +203,7 @@ class Application:
                 self.time_vector[i].grid(row=3, column=i)
                 self.time_matrix[i][j].grid(row=i+5, column=j)
 
-        if self.algo_choice.get() == "Бджолиний":
+        if self.algo_choice.get() != "Жадібний":
             self.l4.grid(row=6+self.Job.number_of_details, column=0)
             self.choice_transit.grid(row=6+self.Job.number_of_details, column=1)
 
@@ -217,7 +219,7 @@ class Application:
             for j in range(self.present_size):
                 self.time_vector[i].grid_forget()
                 self.time_matrix[i][j].grid_forget()
-        if self.algo_choice.get() == "Бджолиний":
+        if self.algo_choice.get() != "Жадібний":
             self.l4.grid_forget()
             self.choice_transit.grid_forget()
         self.draw_input_sheet()
@@ -234,16 +236,30 @@ class Application:
 
             self.Job = Conveyor(number_of_details, spent_time, reload_time)
             if self.algo_choice.get() == "Жадібний":
-                S, z_max = solution.greedy(self.Job)
-                self.output_page(S, z_max)
+                start_time = datetime.now()
+                S_greedy, zmax_greedy = solution.greedy(self.Job)
+                greedy_time = datetime.now() - start_time
+                self.output_page(S_greedy, zmax_greedy, None, None, greedy_time, None)
+            elif self.algo_choice.get() == "Жадібна Бджілка":
+                choice_transit = int(self.choice_transit.get())
+                start_time = datetime.now()
+                S1, z_max1 = solution.bee(self.Job, choice_transit)
+                bee_time = datetime.now() - start_time
+
+                start_time = datetime.now()
+                S2, z_max2 = solution.greedy(self.Job)
+                greedy_time = datetime.now() - start_time
+                self.output_page(S1, z_max1, S2, z_max2, greedy_time, bee_time)
             else:
                 choice_transit = int(self.choice_transit.get())
-                S, z_max = solution.bee(self.Job, choice_transit)
-                self.output_page(S, z_max)
+                start_time = datetime.now()
+                S_bee, zmax_bee = solution.bee(self.Job, choice_transit)
+                bee_time = datetime.now() - start_time
+                self.output_page(None, None, S_bee, zmax_bee, None, bee_time)
         except ValueError:
             self.error_handler("error8")
 
-    def output_page(self, S, z_max):
+    def output_page(self, S_greedy, zmax_greedy, S_bee, zmax_bee, greedy_time, bee_time):
         self.Job.number_of_details = int(self.number_of_details.get())
         self.l1.grid_forget()
         self.l2.grid_forget()
@@ -254,19 +270,72 @@ class Application:
                 self.time_vector[i].grid_forget()
                 self.time_matrix[i][j].grid_forget()
 
-        if self.algo_choice.get() == "Бджолиний":
+        if self.algo_choice.get() != "Жадібний":
             self.l4.grid_forget()
             self.choice_transit.grid_forget()
 
-        self.l1.configure(text="Найкращий росклад:")
-        self.l2.configure(text="Цільова функція:")
-        self.S = Label(self.window, text=str(S))
-        self.z_max = Label(self.window, text=str(z_max))
+        
+        if S_bee == None and zmax_bee == None:
+            self.l1.configure(text="Найкращий росклад:")
+            self.l2.configure(text="Цільова функція:")
+            self.l3.configure(text="Час виконання алгоритму:")
+            self.S = Label(self.window, text=str(S_greedy))
+            self.z_max = Label(self.window, text=str(zmax_greedy))
+            self.greedy_time = Label(self.window, text= str(greedy_time))
+            self.end_button = Button(self.window, text="Ще", command=main)
 
-        self.l1.grid(row=0, column=0)
-        self.S.grid(row=0, column=1)
-        self.l2.grid(row=1, column=0)
-        self.z_max.grid(row=1, column=1)
+            self.l1.grid(row=0, column=0)
+            self.S.grid(row=0, column=1)
+            self.l2.grid(row=1, column=0)
+            self.z_max.grid(row=1, column=1)
+            self.l3.grid(row=2, column=0)
+            self.greedy_time.grid(row=2, column=1)
+            self.end_button.grid(row=3, column=1)
+        elif S_greedy == None and zmax_greedy == None:
+            self.l1.configure(text="Найкращий росклад:")
+            self.l2.configure(text="Цільова функція:")
+            self.S = Label(self.window, text=str(S_bee))
+            self.z_max = Label(self.window, text=str(zmax_bee))
+            self.l3.configure(text="Час виконання алгоритму:")
+            self.bee_time = Label(self.window, text=str(bee_time))
+            self.end_button = Button(self.window, text="Ще", command=main)
+
+            self.l1.grid(row=0, column=0)
+            self.S.grid(row=0, column=1)
+            self.l2.grid(row=1, column=0)
+            self.z_max.grid(row=1, column=1)
+            self.l3.grid(row=2, column=0)
+            self.bee_time.grid(row=2, column=1)
+            self.end_button.grid(row=3, column=1)
+        else:
+            self.l1.configure(text="Найкращий розклад після жадібного алгоритму:")
+            self.l2.configure(text="Цільова функція після жадібного алгоритму:")
+            self.l3.configure(text="Найкращий розклад після бджолиного алгоритму:")
+            self.l4.configure(text="Цільва функція після бджолиного алгоритму:")
+            self.l5 = Label(self.window, text="Час виконання жадібного алгоритму")
+            self.l6 = Label(self.window, text="Час виконання бджолиного алгоритму")
+            self.S_greedy = Label(self.window, text=str(S_greedy))
+            self.S_bee = Label(self.window, text=str(S_bee))
+            self.zmax_greedy = Label(self.window, text=str(zmax_greedy))
+            self.zmax_bee = Label(self.window, text=str(zmax_bee))
+            self.greedy_time = Label(self.window, text=str(greedy_time))
+            self.bee_time = Label(self.window, text=str(bee_time))
+            self.end_button = Button(self.window, text="Ще", command=main)
+
+            self.l1.grid(row=0, column=0)
+            self.S_greedy.grid(row=0, column=1)
+            self.l2.grid(row=1, column=0)
+            self.zmax_greedy.grid(row=1, column=1)
+            self.l3.grid(row=2, column=0)
+            self.S_bee.grid(row=2, column=1)
+            self.l4.grid(row=3, column=0)
+            self.zmax_bee.grid(row=3, column=1)
+            self.l5.grid(row=4, column=0)
+            self.greedy_time.grid(row=4, column=1)
+            self.l6.grid(row=5, column=0)
+            self.bee_time.grid(row=5, column=1)
+            self.end_button.grid(row=6, column=1)
+
 
     def clear_window(self, ivent):
         if ivent == "start":
@@ -289,9 +358,10 @@ class Application:
 
 def main():
     window = Tk()
-    #window.tk.eval('lappend auto_path {%s}' % breeze_theme_path)
-    #theme = ttk.Style()
-    #theme.theme_use('Breeze')
+    window.title("Greedy Bee")
+    style = ThemedStyle(window)
+    style.set_theme("arc")
+
     App = Application(window)
     window.mainloop()
 
